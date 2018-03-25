3cd91e0f64820267261e7eea2833d752198c39b8
Sean Clarke <sean@mpcstudios.com>
2018-03-25 16:19:58 -0500

    add custom action

--- mpc_webhooks/models/sales_order.py
+++ mpc_webhooks/models/sales_order.py
@@ -5,7 +5,7 @@ import json
 _logger = logging.getLogger(__name__)
 
 class sales_order(models.Model):
-	_inherit = 'sales.order'
+	_inherit = 'sale.order'
 
 	@api.multi
 	def action_create_quickbooks(self):
