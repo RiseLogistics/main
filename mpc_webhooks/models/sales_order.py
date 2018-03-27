3c600872e30deecc9b731cc8615553b6abc99f2e
Sean Clarke <sean@mpcstudios.com>
2018-03-27 16:42:13 -0500

    added debug statement

--- mpc_webhooks/models/sales_order.py
+++ mpc_webhooks/models/sales_order.py
@@ -2,6 +2,7 @@ from odoo import fields, models, api
 import logging
 import requests
 import json
+import sys
 _logger = logging.getLogger(__name__)
 
 class sales_order(models.Model):
@@ -11,6 +12,7 @@ class sales_order(models.Model):
 	def action_create_quickbooks(self):
 		_logger.info("Create a copy of this sales order in quickbooks")
 		_logger.info(str(self))
+		_logger.info( str(sys.version) )
 		return {
 			'type': 'ir.actions.act_url',
 			'url': 'https://mpcrequestbin.herokuapp.com/15iix041?id=%s&q=test' % (self.id),
