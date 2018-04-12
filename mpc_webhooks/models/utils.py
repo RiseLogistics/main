e6f3a807ba3d18a95c7750d161f6a09a3a3c72f0
Sean Clarke <sean@mpcstudios.com>
2018-04-12 11:37:02 -0500

    check for staging in webhooks module

--- mpc_webhooks/models/utils.py
+++ mpc_webhooks/models/utils.py
@@ -7,12 +7,16 @@ _logger = logging.getLogger(__name__)
 
 def send_webhook(id,model,trigger):
 	try:
-		webhook_url = "https://sean-local.ngrok.io/api/v3/webhooks/odoo"
-		webhook_url = "https://rise-online.herokuapp.com/api/v3/webhooks/odoo"
+		webhook_url = None
+
+		if "staging" in self._cr.dbname:
+			webhook_url = "https://rise-online.herokuapp.com/api/v3/webhooks/odoo"	
+		else:
+			webhook_url = "https://rise-logistics.herokuapp.com/api/v3/webhooks/odoo"
 
 		payload = {'model':model,'id':id,'trigger':trigger}
 
-		_logger.info("sending webhook notification to: " _ webhook_url)
+		_logger.info("sending webhook notification to: " + webhook_url)
 		_logger.info(payload)
 
 		r = requests.post( webhook_url ,data=json.dumps(payload),headers={'Content-Type': 'application/json'})
