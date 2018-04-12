6bbbf20c758dcdaf0366a48ffa4afc4d799db304
Sean Clarke <sean@mpcstudios.com>
2018-04-12 11:21:16 -0500

    additional logging

--- mpc_webhooks/models/utils.py
+++ mpc_webhooks/models/utils.py
@@ -7,9 +7,14 @@ _logger = logging.getLogger(__name__)
 
 def send_webhook(id,model,trigger):
 	try:
+		webhook_url = "https://sean-local.ngrok.io/api/v3/webhooks/odoo"
+		webhook_url = "https://rise-online.herokuapp.com/api/v3/webhooks/odoo"
+
 		payload = {'model':model,'id':id,'trigger':trigger}
+
+		_logger.info("sending webhook notification to: " _ webhook_url)
 		_logger.info(payload)
-		#r = requests.post( "https://sean-local.ngrok.io/api/v3/webhooks/odoo",data=json.dumps(payload),headers={'Content-Type': 'application/json'})
-		r = requests.post( "https://rise-online.herokuapp.com/api/v3/webhooks/odoo",data=json.dumps(payload),headers={'Content-Type': 'application/json'})
+
+		r = requests.post( webhook_url ,data=json.dumps(payload),headers={'Content-Type': 'application/json'})
 	except Exception as error:
 		_logger.info("error sending webhook: " + str(error))
