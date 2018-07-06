eee971837bc781cd432433c5c842340221f6e955
Yazan <yazan@riselogistics.com>
2018-07-06 18:01:53 -0500

    add timeout try/catch

--- mpc_webhooks/models/utils.py
+++ mpc_webhooks/models/utils.py
@@ -10,7 +10,10 @@ _logger = logging.getLogger(__name__)
 
 def _post(url, payload):
     _logger.info("Calling Webhook [{0}]".format(url))
-    requests.post(url, json=payload, headers={"Content-Type": "application/json"}, timeout=10)
+    try:
+        requests.post(url, json=payload, headers={"Content-Type": "application/json"}, timeout=10)
+    except requests.exceptions.Timeout:
+        _logger.info("Webhook Timeout[{0}]".format(url))
 
 
 def send_webhook(id, model, trigger, dbname=None):
