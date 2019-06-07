e3234bb3743be9e0859477093ccbe2a37d3a7eb6
Yazan <yazan.hussein@dyme.com>
2019-06-07 10:14:03 -0700

    - init module

---
+++ stock_picking_columns/models/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+
+class StockPickingColumns(models.Model):
+    _inherit = "stock.picking"
+
+    dyme_route_schedule = fields.Many2one("Route Schedule",
+                                          related="partner_id.x_route_schedule",
+                                          store=True,
+                                          copy=True,
+                                          readonly=True)
+
+    dyme_partner_region = fields.Selection("Region",
+                                           related="partner_id.x_studio_field_P4UbZ",
+                                           store=True,
+                                           copy=True,
+                                           readonly=True)
+
+
