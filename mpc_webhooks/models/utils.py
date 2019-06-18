5c12fd0b328a9a3f5fba21a1b48ab6305ddb61db
Yazan <yazan.hussein@dyme.com>
2019-06-18 00:33:21 -0700

    rm ll webhook

--- mpc_webhooks/models/utils.py
+++ mpc_webhooks/models/utils.py
@@ -20,21 +20,17 @@ def send_webhook(id, model, trigger, dbname=None):
     try:
         env_type = None
         webhook_url = None
-        leaflink_webhook_url = ("https://leaflink-{0}-riselogistics.herokuapp.com"
-                                "/webhooks/odoo/1RL3137258415e5dee668438702cdd5b24ef158ce5575e")
 
         if "staging" in dbname:
             webhook_url = "https://rise-online.herokuapp.com/api/v3/webhooks/odoo"
-            leaflink_webhook_url = leaflink_webhook_url.format("stage")
             env_type = "[STAGING]"
 
         else:
             webhook_url = "https://rise-logistics.herokuapp.com/api/v3/webhooks/odoo"
-            leaflink_webhook_url = leaflink_webhook_url.format("prod")
             env_type = "[PRODUCTION]"
 
         payload = {'model': model, 'id': id, 'trigger': trigger}
-        urls = [webhook_url, leaflink_webhook_url]
+        urls = [webhook_url]
 
         _logger.info("Payload[{0}] - ENV{1}".format(payload, env_type))
 
